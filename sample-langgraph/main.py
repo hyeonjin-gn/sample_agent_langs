02a966818e4fe45ed25e2ae493737cf85f42e931
hyeonjin-gn <hyeonjin.cho@genesisnest.com>
2025-06-13 20:11:16 +0900

    Add:langfuse 프롬프트 관리 테스트

--- sample-langgraph/main.py
+++ sample-langgraph/main.py
@@ -3,11 +3,33 @@ import os
 from dotenv import load_dotenv
 from state import ChatState
 from langgraph.graph import StateGraph, START, END
-from promptTemplate import prompt, character_name, style
+from promptTemplate import character_name, style
 from node import chat_node
 from langchain_core.messages import HumanMessage, AIMessage
+from langchain.prompts import ChatPromptTemplate, SystemMessagePromptTemplate
+from langfuse.langchain import CallbackHandler
+from langfuse import Langfuse
+
+load_dotenv()
 
 def main():
+    # langfuse 핸들러 초기화
+    langfuse_handler = CallbackHandler()
+    langfuse = Langfuse()
+
+    # langfuse 클라우드에서 production 라벨의 최신 프롬프트 가져오기
+    langfuse_prompt = langfuse.get_prompt("jokerPrompt", label="production")
+    langfuse_prompt_template = langfuse_prompt.get_langchain_prompt()
+
+    print(f"주입받은 lanfuse 프롬프트: {langfuse_prompt_template}")
+
+
+    # 가져온 프롬프트를 langchain 프롬프트로 변경
+    prompt = ChatPromptTemplate.from_messages([
+        SystemMessagePromptTemplate.from_template(langfuse_prompt_template),
+    ]
+    )
+
     builder = StateGraph(ChatState)
     builder.add_node("chat", chat_node)
     builder.add_edge(START, "chat")
@@ -28,7 +50,8 @@ def main():
         human_message = HumanMessage(content=user_input)
         
         state = {"messages": [system_message, human_message]}
-        out = graph.invoke(state)
+        # 랭퓨즈 사용시 invoke에 콜백으로 랭퓨즈 핸들러 전달
+        out = graph.invoke(state, config={"callbacks": [langfuse_handler]})
         print(out["messages"][-1].content)
 
 if __name__ == "__main__":
--- sample-langgraph/node.py
+++ sample-langgraph/node.py
@@ -1,12 +1,15 @@
 from state import ChatState
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import AIMessage
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # LLM 객체를 전역 변수로 한 번만 생성
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 
 # 채팅 노드는 상태를 입력받아 다음 노드에게 상태를 이어 반환합니다. 
-# 노드는 메서드입니다...
+# 노드는 함수입니다...
 def chat_node(state: ChatState) -> ChatState:
     messages = state["messages"]
     response = llm.invoke(messages)
